f1f03685c57c0c4c24e1a39bd1cdcc878362bd0a
Martynjuk Ivan <martiniukgomel@gmail.com>
2024-09-01 20:40:45 +0300

    fix: add  default html meta tags Sun, Sep  1, 2024  8:40:45 PM

--- distance-calculator/src/App.tsx
+++ distance-calculator/src/App.tsx
@@ -1,17 +1,24 @@
 import './app.scss';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import NotFoundPage from './pages/404/404';
-import { selectTheme } from './store/reducers/app-settings-slice';
+import {
+  selectLanguage,
+  selectTheme,
+} from './store/reducers/app-settings-slice';
 import { useAppSelector } from './store/hooks';
 import { useEffect } from 'react';
 import MainPage from './pages/main/main-page';
+import getDocTitle from './common/get-document-title';
 
 function App() {
   const theme = useAppSelector(selectTheme);
+  const lang = useAppSelector(selectLanguage);
 
   useEffect(() => {
     const html = document.documentElement;
     html.setAttribute('data-theme', theme);
+    document.documentElement.lang = lang;
+    document.title = getDocTitle(lang);
   }, [theme]);
 
   return (
